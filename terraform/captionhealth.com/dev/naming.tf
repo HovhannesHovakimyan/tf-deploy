5792ecf74c4ba02dfa0a0ee884c13c6462e0fbca
Hovhannes Hovakimyan <hovhannes.hovakimyan@captionhealth.com>
2024-01-31 11:06:03 +0400

    rg and vnet deleted, naming commented

--- terraform/captionhealth.com/dev/naming.tf
+++ terraform/captionhealth.com/dev/naming.tf
@@ -2,31 +2,31 @@ module "name" {
   source = "../../modules/terraform-azurecaf-name"
 
   azurecaf_name_params = {
-    main_rg = {
-      name           = "dev"
-      prefixes       = ["ch"]
-      suffixes       = ["eastus"]
-      random_length  = 3
-      resource_type  = "azurerm_resource_group"
-      resource_types = null
-      clean_input    = true
-      random_seed    = null
-      separator      = null
-      passthrough    = null
-      use_slug       = null
-    }
-    main_vnet = {
-      name           = "dev"
-      prefixes       = ["ch"]
-      suffixes       = ["eastus"]
-      random_length  = 3
-      resource_type  = "azurerm_virtual_network"
-      resource_types = null
-      clean_input    = true
-      random_seed    = null
-      separator      = null
-      passthrough    = null
-      use_slug       = null
-    }
+    # main_rg = {
+    #   name           = "dev"
+    #   prefixes       = ["ch"]
+    #   suffixes       = ["eastus"]
+    #   random_length  = 3
+    #   resource_type  = "azurerm_resource_group"
+    #   resource_types = null
+    #   clean_input    = true
+    #   random_seed    = null
+    #   separator      = null
+    #   passthrough    = null
+    #   use_slug       = null
+    # }
+    # main_vnet = {
+    #   name           = "dev"
+    #   prefixes       = ["ch"]
+    #   suffixes       = ["eastus"]
+    #   random_length  = 3
+    #   resource_type  = "azurerm_virtual_network"
+    #   resource_types = null
+    #   clean_input    = true
+    #   random_seed    = null
+    #   separator      = null
+    #   passthrough    = null
+    #   use_slug       = null
+    # }
   }
 }
