92be1d6f566ac643d83944a1c3b99d19caea6551
Hovhannes Hovakimyan <hovhannes.hovakimyan@captionhealth.com>
2024-05-27 01:01:17 +0400

    completely reworked workflow, bash script added

---
+++ .github/scripts/find_subfolders.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the root directory
+root_dir="$ROOT_DIR"
+
+# Initialize an empty JSON object
+json_output="{ \"SUBFOLDERS\": {} }"
+
+# Loop through subfolders
+for subfolder in "$root_dir"/*; do
+    if [ -d "$subfolder" ]; then
+        # Extract subfolder name
+        subfolder_name=$(basename "$subfolder")
+
+        # Get absolute path
+        absolute_path=$(realpath "$subfolder")
+
+        # Add subfolder name and absolute path to JSON
+        json_output=$(echo "$json_output" | jq --arg name "$subfolder_name" --arg path "$absolute_path" '.SUBFOLDERS += { ($name): $path }')
+    fi
+done
+
+# Set the JSON output as an environment variable
+echo "export SUBFOLDERS='$json_output'" >> $GITHUB_ENV
